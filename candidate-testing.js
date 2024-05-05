8d00fdbb639ebfbce379ff4394cf14d576e03bb6
Chloe <cmontroy99@gmail.com>
2024-05-05 11:13:54 -0500

    This was fungit status So satistying now that it works!

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
@@ -18,12 +18,17 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name?");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
+    if (candidateAnswer == correctAnswer) { 
+      console.log("Wow, you're smart!");
+   } else {
+        console.log("What a dummy!");
+   }
 
 }
 
@@ -42,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+    console.log(`Hello ${candidateName}! Good luck on your test!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
